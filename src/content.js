333a28c18bd49f3a923c9c6203857b82e5eb96de
Jonathan Hsu <johnnhsu1031@gmail.com>
2023-01-06 17:23:41 -0800

    Add Modal

--- src/content.js
+++ src/content.js
@@ -37,42 +37,94 @@ InboxSDK.load(2, "Hello World!", { timeout: 30000 }).then((sdk) => {
       title: "Generate Email Response",
       iconUrl:
         "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
-      onClick(event) {
+      onClick: function(event){
+        showModal();
+      }
+        // window.open('about:blank', 'Modal Window', 'height=500,width=500,left=100,top=100');
+        
+        // const el = document.createElement('div');
+        // el.innerHTML = 
+        // `<span class="close">&times;</span>
+        // <p>Enter some text:</p>
+        // <input type="text" id="textInput">
+        // <p>Choose an option:</p>
+        // <select id="selectBox">
+        // <option value="option1">Option 1</option>
+        // <option value="option2">Option 2</option>
+        // <option value="option3">Option 3</option>
+        // </select>
+        // <br>
+        // <br>
+        // <button id="button1">Button 1</button>
+        // <button id="button2">Button 2</button>`;
+
+        // sdk.Widget.showModalView({
+        //     title: 'Modal Title',
+        //     el
+        // });
         // Create a form element
-        const form = document.createElement('form');
-        form.innerHTML = `
-          <label for="prompt">What is the email about?</label><br>
-          <input type="text" id="prompt" name="prompt"><br>
-          <input type="submit" value="Generate Email">
-        `;
-        // Add the form to the compose view
-        event.composeView.insertHTMLIntoBodyAtCursor(form);
+        // const form = document.createElement('form');
+        // form.innerHTML = `
+        //   <label for="prompt">What is the email about?</label><br>
+        //   <input type="text" id="prompt" name="prompt"><br>
+        //   <input type="submit" value="Generate Email">
+        // `;
+        // // Add the form to the compose view
+        // event.composeView.insertHTMLIntoBodyAtCursor(form);
 
-        // Add a submit event listener to the form
-        form.addEventListener('submit', async (e) => {
-          e.preventDefault();
-          // Get the value of the prompt input field
-          const prompt = form.querySelector('#prompt').value;
+        // // Add a submit event listener to the form
+        // form.addEventListener('submit', async (e) => {
+        //   e.preventDefault();
+        //   // Get the value of the prompt input field
+        //   const prompt = form.querySelector('#prompt').value;
 
-          // Declare the response variable
-          let response;
-          // Generate the email response
-          response = await generateText(prompt);
-          console.log("m")
-          console.log(response)
-          console.log("i")
+        //   // Declare the response variable
+        //   let response;
+        //   // Generate the email response
+        //   response = await generateText(prompt);
+        //   console.log("m")
+        //   console.log(response)
+        //   console.log("i")
 
-          form.remove();
-          console.log(response)
-          // Insert the email response into the compose view
-          event.composeView.insertHTMLIntoBodyAtCursor(`<div>${response}</div>`);
+        //   form.remove();
+        //   console.log(response)
+        //   // Insert the email response into the compose view
+        //   event.composeView.insertHTMLIntoBodyAtCursor(`<div>${response}</div>`);
 
-          
-          
-        });
-      },
+        // });
+      // },
    
 
     });
   });
 });
+
+function showModal(){
+  InboxSDK.load(2, "Hello World!", { timeout: 30000 }).then((sdk) => {
+    const el = document.createElement('div');
+    el.innerHTML = 
+    `<p>Email Summary:</p>
+    <input type="text" id="textInput">
+    <p>SHow Email Context:</p>
+    <input type="text" id="textInput">
+    <p>Tone:</p>
+    <select id="selectBox">
+    <option value="option1">Default</option>
+    <option value="option2">Happy</option>
+    <option value="option3">Serious</option>
+    </select>
+    <p>Length:</p>
+    <select id="selectBox">
+    <option value="option1">Short</option>
+    <option value="option2">Medium</option>
+    <option value="option3">Long</option>
+    </select>
+    <br>
+    <br>
+    <button id="Generate Email">Generate Response</button>`;
+    sdk.Widgets.showModalView({
+        title: 'Filos',
+        el
+    });
+  });
+}
